e2dd76b8a6bf9b313ad1f82ef179e5f782bbdfc8
Jacob Valdez <jacobfv123@gmail.com>
2024-05-12 14:48:46 -0500

    updated config

--- app/__main__.py
+++ app/__main__.py
@@ -27,9 +27,6 @@ import os
 load_dotenv()
 
 stripe.api_key = os.getenv("STRIPE_API_KEY")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 JWT_SECRET = os.getenv("JWT_SECRET")
 
 app = FastAPI()
