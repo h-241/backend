b6b9a1e100bc694aaa021fcce49481e3e213a10d
Jacob Valdez <jacobfv123@gmail.com>
2024-05-12 08:36:14 -0500

    base interface

--- h241_contract/main.py
+++ h241_contract/main.py
@@ -1,61 +1,62 @@
 from __future__ import annotations
 
-from pydantic import BaseModel
+from pydantic import UUID4, BaseModel
 
 
-class ServiceProvider(BaseModel):
+class Worker(BaseModel):
     name: str
     bio: str
-    executions: list[Execution]
+    executions: list[Task]
     
     recovery_email: str
     icp_ledger_account_id: str
-    min_service_request_duration: int
-    min_service_request_price: int
+    
+    min_task_duration: int
+    min_task_price: int
 
 
-class ServiceRequest(BaseModel):
-    job_description: str
+class TaskRequest(BaseModel):
+    description: str
     
-    max_duration_to_match_with_service_provider_until_expiration: int
-    max_duration_for_service_provider_to_complete_work_until_expiration: int
+    match_expiration_duration: int
+    completion_expiration_duration: int
     
-    completion_criteria: str
-    payment_criteria: str
     max_price: int
     min_price: int
 
 
-class Execution(BaseModel):
+class Task(BaseModel):
     # we'll carbon copy this onto the chain
     
-    service_request: ServiceRequest
-    service_provider: ServiceProvider
+    task_request: TaskRequest
+    task_provider: Worker
     start_time_ns: int
     end_time_ns: int
     
-    amount_reserved: int
-    reservation_taken_from_icp_ledger_account_id: str
+    amount_escrowed: int
+    escrow_source: str
     amount_paid: int
     paid_to_icp_ledger_account_id: str
     
     duration: int
 
-# TODO: also make it possible to see the realtime threads of all available jobs
+# How do i authenticate identifty?
 
-# TODO: most of this goes on the couchbase. only a small amount on the icp. maybe we don't store anything on the icp. just use it for tokenomics
-
-# TODO: make the execution have a chat betwen the provider and consumer. the completion criteria can require the human to approve the work (not that this is not the only payment scheme, and it isn't in the core idea of blockchain so many will not do this)
-
-def request_service(service_request, requester) -> ServiceRequest:
+def create_task_request(task_request_data: TaskRequest) -> UUID4:
     ...
 
-def present_evidence_of_completion(evidence, service_request, service_provider) -> ServiceRequest:
+def get_available_task_requests(task_provider_id: UUID4) -> list[TaskRequest]:
     ...
 
-def register_service_provider(service_provider) -> ServiceProvider:
+def accept_task_request(task_request_id: UUID4) -> UUID4:
     ...
 
-def cancel_execution(execution) -> Execution:
+def add_message_to_task(task_id: UUID4, message: str):
+    ...
+
+def get_messages_for_task(task_id: UUID4) -> list[str]:
+    ...
+
+def cancel_task(task_id: UUID4):
     ...
 
